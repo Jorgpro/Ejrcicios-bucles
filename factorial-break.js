a740691e01775076592ba76bb848beb9f70849fe
Jorgpro <115546930+Jorgpro@users.noreply.github.com>
2022-11-27 23:29:57 -0300

    Create factorial-break.js

---
+++ factorial-break.js
@@ -0,0 +1,14 @@
+let b = 0;
+   let maximo = 12;
+   
+      while(b < maximo ){
+   console.log(b)
+     b++
+
+
+if ( b > 10){
+    console.log("Se detiene en este instante")
+break;
+}
+      }
+   
